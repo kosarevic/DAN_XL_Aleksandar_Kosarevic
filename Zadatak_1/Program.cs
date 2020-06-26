539a43117f44118e7f7bcc9f098e26a878a5c39b
Kosarevic <kosarevic@gmail.com>
2020-06-26 14:30:57 +0200

    Added comments to the code.

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -8,18 +8,24 @@ using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
+    /// <summary>
+    /// Application simulates printing requests made by 10 computers.
+    /// </summary>
     class Program
     {
+        //Necesery variables for application functionality.
         public static List<string> colors = new List<string>();
         public static Random r = new Random();
         public static bool all = false;
 
         static void Main(string[] args)
         {
+            //Colors are being added from palette.txt file.
             Read();
-
+            //Loop produces threads randomly with each iteration.
             while (!all)
             {
+                //Elements of Printer object are assigned random values.
                 string[] format = new string[2];
                 format[0] = "A3";
                 format[1] = "A4";
@@ -30,29 +36,33 @@ namespace Zadatak_1
                 p.Format = format[r.Next(0, 2)];
                 p.Color = Program.colors[r.Next(0, Program.colors.Count())];
                 p.Orientation = orientation[r.Next(0, 2)];
-
+                //Threads are produced and name is added randomly.
                 Thread t = new Thread(() => Service.Pc(p));
                 t.Name = string.Format("PC_{0}", r.Next(1,11));
-
+                //When thread is made, it is displayed on the console.
                 Console.WriteLine("{0} has sent {1} format print request.", t.Name, p.Format);
                 Console.WriteLine("color: {0}, orientation: {1}.", p.Color, p.Orientation);
-
+                //Condition checks if semaphore in consequential methods has avalaible slot.
                 if (Service.smp1.CurrentCount == 1 && Service.smp2.CurrentCount == 1 && !all)
                 {
+                    //If so, thread may start.
                     t.Start();
                 }
+                //Sleep added to pause threat generation process for short time.
                 Thread.Sleep(100);
             }
-
+            //Reset handle waits for last threads to complete before allowing final code to execute.
             Service.ewh.WaitOne();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine();
-            Console.WriteLine("All 10 PC have printed at least once.");
+            Console.WriteLine("All 10 PC-s have printed at least once.");
             Console.ResetColor();
 
             Console.ReadLine();
         }
-
+        /// <summary>
+        /// Method reads colors from file, and adds them to the list.
+        /// </summary>
         static void Read()
         {
             StreamReader sr = new StreamReader("..//../Files/Palette.txt");
--- Zadatak_1/Service.cs
+++ Zadatak_1/Service.cs
@@ -9,11 +9,17 @@ namespace Zadatak_1
 {
     class Service
     {
+        //Semaphore made to ensure only one thread may execute coresponding part of code.
         public static SemaphoreSlim smp1 = new SemaphoreSlim(1);
         public static SemaphoreSlim smp2 = new SemaphoreSlim(1);
+        //Bool values made to keep track of every pc heaving printed at least once.
         public static bool pc1, pc2, pc3, pc4, pc5, pc6, pc7, pc8, pc9, pc10 = false;
+        //Event handle tracks when printing proces completes.
         public static EventWaitHandle ewh = new AutoResetEvent(false);
-
+        /// <summary>
+        /// Method purpose is to separate printing requests based on format chosen.
+        /// </summary>
+        /// <param name="p"></param>
         public static void Pc(Printer p)
         {
             if (p.Format == "A3")
@@ -25,37 +31,56 @@ namespace Zadatak_1
                 PrintA4(p);
             }
         }
-
+        /// <summary>
+        /// Method simulates printing of A3 size document.
+        /// </summary>
+        /// <param name="printer"></param>
         public static void PrintA3(Printer printer)
         {
+            //Semaphore applyed.
             smp1.Wait();
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Printing A3, for {0}...", Thread.CurrentThread.Name);
             Console.ResetColor();
             Thread.Sleep(1000);
+            //Delegate called.
             PrintingDoneA3(Thread.CurrentThread.Name);
+            //Validation checked.
             ValidatePC(Thread.CurrentThread.Name);
             smp1.Release();
+            //Block event removed.
             ewh.Set();
         }
-
+        /// <summary>
+        /// Method simulates printing of A4 sized document.
+        /// </summary>
+        /// <param name="printer"></param>
         public static void PrintA4(Printer printer)
         {
+            //Semaphore applyed.
             smp2.Wait();
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Printing A4, for {0}...", Thread.CurrentThread.Name);
             Console.ResetColor();
             Thread.Sleep(1000);
+            //Delegate called.
             PrintingDoneA4(Thread.CurrentThread.Name);
+            //Validation checked.
             ValidatePC(Thread.CurrentThread.Name);
             smp2.Release();
+            //Block event removed.
             ewh.Set();
         }
-
+        /// <summary>
+        /// Delegate and Notification for signaling printing completion.
+        /// </summary>
         public delegate void Notification();
 
         public static event Notification OnNotification;
-
+        /// <summary>
+        /// Completion notification for A3 format document.
+        /// </summary>
+        /// <param name="format"></param>
         public static void PrintingDoneA3(string format)
         {
             OnNotification = () =>
@@ -66,7 +91,10 @@ namespace Zadatak_1
             };
             OnNotification.Invoke();
         }
-
+        /// <summary>
+        /// Completion notification for A4 format document.
+        /// </summary>
+        /// <param name="format"></param>
         public static void PrintingDoneA4(string format)
         {
             OnNotification = () =>
@@ -77,7 +105,10 @@ namespace Zadatak_1
             };
             OnNotification.Invoke();
         }
-
+        /// <summary>
+        /// Method checks if pc had at least one printing of document, and sets coresponding bool value.
+        /// </summary>
+        /// <param name="s"></param>
         public static void ValidatePC(string s)
         {
             if(s == "PC_1") { pc1 = true; }
@@ -90,7 +121,7 @@ namespace Zadatak_1
             else if (s == "PC_8") { pc8 = true; }
             else if (s == "PC_9") { pc9 = true; }
             else if (s == "PC_10") { pc10 = true; }
-            
+            //If all PC-s had at least one printing, application will end with bool "all" set to True.
             if(pc1 && pc2 && pc3 && pc4 && pc5 && pc6 && pc7 && pc8 && pc9 && pc10)
             {
                 Program.all = true;
